8dcc27e912d0534bf3a92b30c26cdc64d5c18c8e
Zolastic <honhattienlul@gmail.com>
2023-07-31 19:21:37 +0800

    Add the APP_PROFILE for the Database Connection

--- src/main/java/utils/DBConnection.java
+++ src/main/java/utils/DBConnection.java
@@ -5,7 +5,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/jad_ca1?user=root&password=root&serverTimezone=UTC";
+    private static final String DB_URL;
+    static {
+    	String profile = System.getenv("APP_PROFILE");
+    	
+    	if ("cloud".equals(profile)) {
+    		DB_URL = "jdbc:mysql://inkwell.mysql.database.azure.com:3306/jad_ca2?user=jad&password=ca2123!?&serverTimezone=UTC";
+    	} else {
+    		DB_URL = "jdbc:mysql://localhost:3306/jad_ca1?user=root&password=root&serverTimezone=UTC";
+    	}
+    	
+    }
+    
     
     static {
     	try {
